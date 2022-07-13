d20b9af615130f1c289ff44e5e5899334e681894
seo722 <95078290+seo722@users.noreply.github.com>
2022-07-13 13:04:06 +0900

    sidebar start

---
+++ components/Sidebar.js
@@ -0,0 +1,19 @@
+import Image from "next/image";
+
+function Sidebar() {
+  return (
+    <div
+      className="hidden sm:flex flex-col items-center xl:items-start
+      xl:w-[340px] p-2 fixed h-full"
+    >
+      <div
+        className="flex items-center justify-center w-14 h-14
+      hoverAnimation p-0 xl:ml-24"
+      >
+        <Image src="http://rb.gy/ogau5a" width={30} height={30} />
+      </div>
+    </div>
+  );
+}
+
+export default Sidebar;
