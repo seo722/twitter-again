7c135a9e61170c7a817153d9acbcd3a5b86b5dc8
seo722 <95078290+seo722@users.noreply.github.com>
2022-07-21 14:09:59 +0900

    sidebar style done

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,5 +1,16 @@
 import Image from "next/image";
 import SidebarLink from "./SidebarLink";
+import {
+  BellIcon,
+  BookmarkIcon,
+  ClipboardListIcon,
+  DotsCircleHorizontalIcon,
+  DotsHorizontalIcon,
+  HashtagIcon,
+  HomeIcon,
+  InboxIcon,
+  UserIcon,
+} from "@heroicons/react/solid";
 
 function Sidebar() {
   return (
@@ -14,7 +25,36 @@ function Sidebar() {
         <Image src="http://rb.gy/ogau5a" width={30} height={30} />
       </div>
       <div className="space-y-2.5 mt-4 mb-2.5 xl:ml-24">
-        <SidebarLink />
+        <SidebarLink text="Home" Icon={HomeIcon} active />
+        <SidebarLink text="Explore" Icon={HashtagIcon} />
+        <SidebarLink text="Notifications" Icon={BellIcon} />
+        <SidebarLink text="Messages" Icon={InboxIcon} />
+        <SidebarLink text="Bookmarks" Icon={BookmarkIcon} />
+        <SidebarLink text="Lists" Icon={ClipboardListIcon} />
+        <SidebarLink text="Profile" Icon={UserIcon} />
+        <SidebarLink text="More" Icon={DotsCircleHorizontalIcon} />
+      </div>
+      <button
+        className="hidden xl:inline ml-auto bg-[#1d9bf0] text-white
+        rounded-full w-56 h-[52px] text-lg font-bold shadow-md hover:bg-[#1a8cd8]"
+      >
+        Tweet
+      </button>
+      <div
+        className="text-[#d9d9d9] flex items-center justify-center
+        mt-auto hoverAnimation xl:ml-auto "
+        // xl:-mr-5
+      >
+        <img
+          src="https://pbs.twimg.com/media/FX9kIDvaMAAzqYl?format=jpg"
+          alt=""
+          className="h-10 w-10 rounded-full xl:mr-2.5"
+        />
+        <div className="hidden xl:inline leading-5">
+          <h4 className="font-bold">Vox Akuma</h4>
+          <p className="text-[#6e767d]">@voxakuma</p>
+        </div>
+        <DotsHorizontalIcon className="h-5 hidden xl:inline ml-10" />
       </div>
     </div>
   );
