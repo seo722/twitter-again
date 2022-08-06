95a99ecb9bbaa9ff09243c40b9618de154cf095a
seo722 <95078290+seo722@users.noreply.github.com>
2022-08-06 15:03:38 +0900

    input box & icons

--- components/Input.js
+++ components/Input.js
@@ -1,5 +1,90 @@
+import {
+  CalendarIcon,
+  ChartBarIcon,
+  EmojiHappyIcon,
+  PhotographIcon,
+  XIcon,
+} from "@heroicons/react/outline";
+import { useRef, useState } from "react";
+
 function Input() {
-  return <div>Input</div>;
+  const [input, setInput] = useState("");
+  const [selectedFile, setSelectedFile] = useState(null);
+  const filePickerRef = useRef(null);
+
+  const addImageToPost = () => {};
+
+  return (
+    <div
+      className={`border-b border-gray-700 p-3 flex space-x-3
+      overflow-y-scroll scrollbar-hide`}
+    >
+      <img
+        src="https://pbs.twimg.com/media/FX9kIDvaMAAzqYl?format=jpg"
+        alt=""
+        className="h-11 w-11 rounded-full cursor-pointer"
+      />
+      <div className="w-full divide-y divide-gray-700">
+        <div className={``}>
+          <textarea
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            rows="2"
+            placeholder="What's happening?"
+            className="bg-transparent outline-none text-[#d9d9d9]
+            text-lg placeholder-gray-500 tracking-wide w-full min-h-[50px]"
+          />
+
+          {/* 사진 파일 선택한 경우 */}
+          {selectedFile && (
+            <div className="relative">
+              <div
+                className="absolute w-8 h-8 bg-[#15181c] hover:bg-[#272c26]
+                bg-opacity-75 rounded-full flex items-center justify-center top-1
+                left-1 cursor-pointer"
+                onClick={() => setSelectedFile(null)}
+              >
+                <XIcon className="text-white h-5" />
+              </div>
+              <img
+                src={selectedFile}
+                alt=""
+                className="rounded-2xl max-h-80 object-contain
+            "
+              />
+            </div>
+          )}
+        </div>
+
+        {/* icons */}
+        <div className="flex items-center justify-between pt-2.5">
+          <div className="flex items-center">
+            <div className="icon" onClick={() => filePickerRef.current.click()}>
+              <PhotographIcon className="h-[22px] text-[#1d9bf0]" />
+              <input
+                type="file"
+                hidden
+                onChange={addImageToPost}
+                ref={filePickerRef}
+              />
+            </div>
+
+            <div className="icon rotate-90">
+              <ChartBarIcon className="text-[#1d9bf0] h-[22px]" />
+            </div>
+
+            <div className="icon">
+              <EmojiHappyIcon className="text-[#1d9bf0] h-[22px]" />
+            </div>
+
+            <div className="icon">
+              <CalendarIcon className="text-[#1d9bf0] h-[22px]" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Input;
